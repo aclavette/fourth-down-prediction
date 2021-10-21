a185003c266cf1cba0db29dede455eba1a59bd64
aclavette <78436534+aclavette@users.noreply.github.com>
2021-10-20 20:03:58 -0400

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,17 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 import pandas as pd
-dataset= pd.read_csv('data.csv')
 
+dataset= pd.read_csv('data.csv')
 df = pd.DataFrame(dataset)
 
 #dataframe forgets about NA rows
 df = df[ df['down'].notna()]
-#print(df)
-
 df.to_csv(r'C:\Users\aclav\OneDrive\Desktop\CS368\newdata.csv', index = False, header=True)
