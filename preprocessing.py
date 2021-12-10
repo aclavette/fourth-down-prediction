47374c81f9078fedca36187a6083669080f94cae
aclavette <78436534+aclavette@users.noreply.github.com>
2021-12-09 22:12:28 -0500

    delete file

--- preprocessing.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-dataset= pd.read_csv('data.csv')
-df = pd.DataFrame(dataset)
-
-#dataframe forgets about NA rows
-df = df[ df['down'].notna()]
-df.to_csv(r'C:\Users\aclav\OneDrive\Desktop\CS368\newdata.csv', index = False, header=True)
